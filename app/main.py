137119d98430ec850c956588728920ee99e5c95c
aryan <notlelouchvb@gmail.com>
2023-10-04 05:17:58 +0530

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -8,10 +8,8 @@ def main():
         client_socket, client_address = server_socket.accept()# wait for client
 
         request_data = client_socket.recv(1024).decode("utf-8")
-        response = "HTTP/1.0 200 OK\r\n\r\nHello Aryan!! and duniya ki ma ki chutttt!!"
-        #response = "HTTP/1.0 200 OK\r\n\r\n duniya ki ma ki chutttttttt!!!!!!!!"
+        response = "HTTP/1.0 200 OK\r\n\r\nHello Aryan!!"
 
-        print("ig it worked somehow, i really don't undertand it much yet")
         client_socket.send(response.encode("utf-8"))
         client_socket.close()
         
