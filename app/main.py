5ca8ce0c2c1c171de0183158d882d978c1267eb0
aryan <notlelouchvb@gmail.com>
2023-10-04 05:44:03 +0530

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -2,25 +2,17 @@ import socket
 
 def main():
     print("Logs from your program will appear here!")
-
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(("localhost", 4221))
-    server_socket.listen(1)
-
-    print("Server listening on port 4221")
+    
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
 
     while True:
-        client_socket, client_address = server_socket.accept()
-        print(f"Accepted connection from {client_address}")
+        client_socket, client_address = server_socket.accept()# wait for client
 
-        # Read the HTTP request from the client
         request_data = client_socket.recv(1024).decode("utf-8")
+        response = "HTTP/1.0 200 OK\r\n\r\nHello Aryan!!"
 
-        # Respond with a 200 OK HTTP response
-        response = "HTTP/1.1 200 OK\r\n\r\nHello, World!"
         client_socket.send(response.encode("utf-8"))
-
         client_socket.close()
-
+        
 if __name__ == "__main__":
     main()
