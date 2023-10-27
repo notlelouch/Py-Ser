a3e3f563fd4b5125298f58e5ec214669e51c99ee
aryan <notlelouchvb@gmail.com>
2023-10-28 01:00:22 +0530

    responding with content1283

--- app/main.py
+++ app/main.py
@@ -1,16 +1,16 @@
 import socket
 import threading
 
-def handle_client(client_socket):
-    # Existing code for handling a single clieent
+def handle_client(client_socket, directory):
+    # Existing code for handling a single client
     request_data = client_socket.recv(1024).decode("utf-8")
-    response = handle_request(request_data)
+    response = handle_request(request_data, directory)
     if response is not None:
         client_socket.send(response.encode("utf-8"))
     client_socket.close()
 
 
-def handle_request(request_data):
+def handle_request(request_data, directory):
     # Split the request data into lines
     lines = request_data.strip().split("\r\n")
 
@@ -46,6 +46,7 @@ def handle_request(request_data):
     # Process the request based on the path
     if path == "/":
         response = "HTTP/1.1 200 OK\r\n\r\nHello, World!"
+
     elif path.startswith("/echo/"):
         # Get the echo content from the path
         echo_content = path[len("/echo/"):]
@@ -54,6 +55,7 @@ def handle_request(request_data):
         response += f"Content-Length: {len(echo_content)}\r\n"
         response += "\r\n"  # End of headers
         response += echo_content  # Response content
+
     elif path.startswith("/user-agent"):
         # Get the echo content from the path
         echo_content = headers["User-Agent"]
@@ -62,6 +64,24 @@ def handle_request(request_data):
         response += f"Content-Length: {len(echo_content)}\r\n"
         response += "\r\n"  # End of headers
         response += echo_content 
+
+    elif path.startswith("/files/"):
+        # Extract the filename from the path
+        filename = path[len("/files/"):]
+
+        # Build the full path to the file
+        file_path = os.path.join(directory, filename)
+
+        if os.path.exists(file_path) and os.path.isfile(file_path):
+            with open(file_path, 'rb') as file:
+                file_content = file.read()
+                response = "HTTP/1.1 200 OK\r\n"
+                response += "Content-Type: application/octet-stream\r\n"
+                response += f"Content-Length: {len(file_content)}\r\n"
+                response += "\r\n"  # End of headers
+                response = response.encode("utf-8") + file_content
+        else:
+            response = "HTTP/1.1 404 Not Found\r\n\r\nFile not found"
     else:
         response = "HTTP/1.1 404 Not Found\r\n\r\nPage not found"
 
@@ -71,7 +91,8 @@ def main():
     print("Logs from your program will appear here!")
     
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-
+    directory = "/tmp/data/codecrafters.io/http-server-tester/" 
+    
     while True:
         client_socket, client_address = server_socket.accept()# wait for client
         client_thread = threading.Thread(target=handle_client, args=(client_socket,))
